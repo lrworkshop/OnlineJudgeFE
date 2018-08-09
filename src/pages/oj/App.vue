8a23d72702ae4b46a770d6555a7198b5a623228e
开水专烫鸡 <gyczt879574764@gmail.com>
2018-08-09 10:12:29 +0800

    test

--- src/pages/oj/App.vue
+++ src/pages/oj/App.vue
@@ -8,8 +8,7 @@
       </transition>
       <div class="footer">
         <p v-html="website.website_footer"></p>
-        <p>Powered by <a href="https://github.com/QingdaoU/OnlineJudge">OnlineJudge</a>
-          <span v-if="version">&nbsp; Version: {{ version }}</span>
+        <p>hkjnasdfklasdhfaldfjalsfasljdfajldfiasfad
         </p>
       </div>
     </div>
